542f6e846022aa064e774edec66af3656c45136b
DESKTOP-U40EMUD\joowo <joowonkoh0505@gmail.com>
2024-05-12 18:27:45 +0900

    FP-230 feat : config 파일 작성

--- LUFFY/src/main.jsx
+++ LUFFY/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, redirect, RouterProvider } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./utils/query.js";
+
+//pages
+import RootLayout from "./pages/RootLayout.jsx";
+
+
+
+
+const router = createBrowserRouter([
+	// 메인 페이지
+	{
+		path: "/",
+		element: <RootLayout />,
+  }
+])
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
+
--- LUFFY/vite.config.js
+++ LUFFY/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import million from "million/compiler";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/",
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  plugins: [million.vite({ auto: true }), react(), svgr()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  assetsInclude: ["**/*.jpg"],
+});
