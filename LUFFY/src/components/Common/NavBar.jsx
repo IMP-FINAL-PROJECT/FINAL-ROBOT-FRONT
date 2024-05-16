f3f28286e0414ec7b7f00f598ebf297f9ef22379
DESKTOP-U40EMUD\joowo <joowonkoh0505@gmail.com>
2024-05-16 23:03:55 +0900

    FP-267 feat : 메인 화면 nav 바 UI변경

--- LUFFY/src/components/Common/NavBar.jsx
+++ LUFFY/src/components/Common/NavBar.jsx
@@ -1,62 +1,56 @@
 import React from 'react';
-import MainLogo from "../../assets/MainLogo.svg";
+import MainLogo from "../../assets/MainLogo.svg?react";
 import Luffy_Call_Icon from "../../assets/Icon/Luffy_Call.svg?react";
 import Diary_Icon from "../../assets/Icon/Diary.svg?react";
 import Luffy_Icon from "../../assets/Icon/Luffy.svg?react";
 import Quotes_Icon from "../../assets/Icon/Quotes.svg?react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./NavBar.css"
 
 export default function NavBar() {
-  // 기본 네비게이션 바 CSS
-  const baseNavbarCss = "flex justify-start gap-8 mx-[2.4rem] flex items-center gap-4";
-
-  // 활성화된 링크의 CSS
+  const baseNavbarCss = "flex flex-col justify-center items-center gap-1 mx-[2.4rem] tooltip";
   const activeNavbarCss = `${baseNavbarCss} text-lightpurple fill-lightpurple`;
-
-  // 비활성화된 링크의 CSS
   const inactiveNavbarCss = `${baseNavbarCss} hover:text-lightpurple hover:fill-lightpurple fill-text-gray`;
+  const navTextCss = "text-[1vw] font-bold"; // Smaller text size
+  const quoteTextCss = "text-[0.95vw] font-bold"; // Slightly smaller text size for the quotes icon
 
-  // 네비게이션 텍스트 스타일
-  const navTextCss = "text-[1.75rem] font-bold";
-  
-  // 오늘의명언 텍스트 크기
-  const quoteTextCss = "text-[1.70rem] font-bold";
+  const navItems = [{
+          link: "/",
+          icon: MainLogo,
+          label: "홈",
+          isMainLogo: true
+        }, {
+          link: "luffycall",
+          icon: Luffy_Call_Icon,
+          label: "러피콜"
+        }, {
+          link: "diary",
+          icon: Diary_Icon,
+          label: "상담기록"
+        }, {
+          link: "luffy",
+          icon: Luffy_Icon,
+          label: "러피"
+        }, {
+          link: "today",
+          icon: Quotes_Icon,
+          label: "오늘의명언"
+        }];
 
   return (
-    <div className="col-span-2 h-screen bg-beige relative">
-      <Link to="/">
-        <img src={MainLogo} alt="Main logo" className="mb-0" />
-      </Link>
-      <div className="grid grid-rows-6 gap-[1.5vw] mt-[192px] bg-beige" id="navbar-main">
-        {/* 각 NavLink 컴포넌트를 랜더링합니다. isActive prop을 통해 스타일을 조정합니다. */}
-        <NavLink
-          to="luffycall"
-          className={({ isActive }) => (isActive ? activeNavbarCss : inactiveNavbarCss)}
-        >
-          <Luffy_Call_Icon style={{ width: 80, height: 80 }}/>
-          <p className={navTextCss}>러피콜</p>
-        </NavLink>
-        <NavLink
-          to="diary"
-          className={({ isActive }) => (isActive ? activeNavbarCss : inactiveNavbarCss)}
-        >
-          <Diary_Icon style={{ width: 80, height: 80 }}/>
-          <p className={navTextCss}>상담기록</p>
-        </NavLink>
-        <NavLink
-          to="luffy"
-          className={({ isActive }) => (isActive ? activeNavbarCss : inactiveNavbarCss)}
-        >
-          <Luffy_Icon style={{ width: 80, height: 80 }}/>
-          <p className={navTextCss}>러피</p>
-        </NavLink>
-        <NavLink
-          to="today"
-          className={({ isActive }) => (isActive ? activeNavbarCss : inactiveNavbarCss)}
-        >
-          <Quotes_Icon style={{ width: 80, height: 80 }}/>
-          <p className={quoteTextCss}>오늘의명언</p>
-        </NavLink>
+    <div className="col-span-2 h-screen bg-gradient-to-r from-beige to-beige20 relative shadow">
+      <div className="grid grid-rows-6 gap-[0.7vw] mt-[2vw]" id="navbar-main">
+        {navItems.map(({ link, icon: Icon, label, isMainLogo }) => (
+          <Link
+            to={link}
+            className={inactiveNavbarCss}
+          
+            key={link}
+          >
+            <Icon className="rounded-[20px] shadow-md w-[7vw] h-[7vw]"/>
+            <p className={navTextCss}>{label}</p>
+          </Link>
+        ))}
       </div>
     </div>
   );
