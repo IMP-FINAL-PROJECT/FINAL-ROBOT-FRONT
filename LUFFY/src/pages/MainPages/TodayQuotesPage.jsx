7a6045f8350d1c2e22be90440c749e57f880bbb8
DESKTOP-U40EMUD\joowo <joowonkoh0505@gmail.com>
2024-05-20 23:48:24 +0900

    FP-293 feat : 오늘의 명언 페이지 구성

---
+++ LUFFY/src/pages/MainPages/TodayQuotesPage.jsx
@@ -0,0 +1,116 @@
+import dayjs from "dayjs";
+import TodayRecoding from "../../components/TodayQuotes/TodayRecoding";
+import ModalRoute from "../../components/Modal/ModalRoute";
+import NextIcon from "../../assets/NextIcon.svg";
+import { useState,useEffect } from "react";
+import TodayQuotesBox from "../../components/TodayQuotes/TodayQuotesBox";
+import useCounselStore from "../../stores/useCounselStore";
+import useUserStore from "../../stores/useUserStore";
+import { useQuery } from "@tanstack/react-query";
+import { getAnsList } from "../../services/quoteService";
+
+export default function TodayQuotesPage() {
+  const [isquoteServiceBoxOpen, setIsquoteServiceBoxopen] = useState(false);
+  const [isAnswered, setIsAnswered] = useState(false);
+  const [isRecoding, setIsRecoding] = useState(false);
+  const[loading, setLoading] = useState(true);
+  const [nicknameCount, setNicknameCount] = useState(0);
+  const { quote } = useCounselStore();
+  const date = dayjs();
+  const year = date.get("y");
+  const month = date.get("M") + 1;
+  const day = date.get("D");
+  const { counselId } = useCounselStore();
+  const {nickname} = useUserStore();
+  const { data: ansList, isLoading } = useQuery({
+    queryKey: ["ansList", counselId],
+    queryFn: () => getAnsList(counselId),
+  });
+  useEffect(() => {
+    if(!isLoading) {
+      setLoading(true);
+    let count = 0;
+    // 모든 답변 그룹을 반복
+    ansList.answerList.forEach(group => {
+        // 각 그룹 내 답변을 반복
+        group.forEach(answer => {
+            if (answer.nickname === nickname) {
+                count++;
+            }
+        });
+    });
+    setNicknameCount(count);
+    setLoading(false);
+  }
+}, [isLoading]);
+
+  const handleClickIsquoteBoxOpen = () => {
+    if (!isAnswered) {
+      setIsquoteServiceBoxopen((pre) => !pre);
+    } else {
+      alert("상대방이 아직 답변을 작성하지 않았어요! 조금 기다릴까요?");
+    }
+  };
+
+  const handleIsAnswered = () => {
+    setIsAnswered((pre) => true);
+  };
+
+  const handleIsRecording = () => {
+    setIsRecoding((pre) => !pre);
+  };
+
+  if (isLoading || loading===true) {
+    return <div>로딩 중</div>;
+  }
+  
+  return (
+    <ModalRoute
+      modalcss="w-[49.5vw] h-[85vh] bg-pink rounded-[40px]"
+      isX={true}
+    >
+      <p className="text-[1.8vw] ml-[4vw] mt-[2vw] text-text-black">
+        {year}년 {month}월 {day}일
+      </p>
+      <div className="flex flex-col items-center justify-center mx-auto">
+        <button
+          onClick={handleClickIsquoteBoxOpen}
+          disabled={isRecoding}
+          className="bg-white h-[3vw] w-[42vw] mt-[0.5vw] rounded-[35px] shadow-md leading-[3vw] grid grid-cols-10"
+        >
+          {console.log('asdasd'+nicknameCount)}
+          <p className="text-[1.3vw] col-span-9 text-text-black hover:text-[#888888]">
+          {nicknameCount==(ansList.answerList).length? "기록": isquoteServiceBoxOpen ? "기록" : "오늘의 명언 "}
+          </p>
+          <div className="col-span-1 h-[2.5vw] my-auto">
+            <img className="m-auto w-full h-full" src={NextIcon} alt="" />
+          </div>
+        </button>
+
+        {nicknameCount==(ansList.answerList).length?  (<TodayQuotesBox
+        handleIsAnswered={handleIsAnswered}
+        isAnswered={isAnswered}
+        ansList={ansList}
+        handleClickIsquoteBoxOpen={handleClickIsquoteBoxOpen}
+      />
+        ): !isquoteServiceBoxOpen ? (
+          <div className=" w-full flex flex-col items-center justify-center">
+          <p className="w-[80%] mt-[2vw] text-[1.5vw] col-span-9 text-text-black ">
+          {quote.content}
+          </p>
+          <TodayRecoding
+            handleIsRecording={handleIsRecording}
+            handleClickIsquoteBoxOpen={handleClickIsquoteBoxOpen}
+            handleIsAnswered={handleIsAnswered}
+          />
+          </div>
+        ): <TodayQuotesBox
+        handleIsAnswered={handleIsAnswered}
+        isAnswered={isAnswered}
+        ansList={ansList}
+        handleClickIsquoteBoxOpen={handleClickIsquoteBoxOpen}
+      />}
+      </div>
+    </ModalRoute>
+  );
+}
